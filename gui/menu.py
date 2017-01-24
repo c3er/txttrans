ff72026e9affa701ac144bc6d0fb9451054a8f6e
Christian Dreier <flipper.x3r@googlemail.com>
2017-01-24 23:02:50 +0100

    Fix typo

--- gui/menu.py
+++ gui/menu.py
@@ -70,7 +70,7 @@ class Popup:
         
     def dispatch(self, label):
         """Calls the handler.
-        The event that is given to the handler is the appeareance of the menu
+        The event that is given to the handler is the appearance of the menu
         and not the actual call of the menu item.
         """
         self.handlers[label](self.lastevent)
