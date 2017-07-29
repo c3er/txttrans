68e822871bfb7c8be81f0f84be20c31f71e97061
Christian Dreier <flipper.x3r@googlemail.com>
2017-07-29 21:00:30 +0200

    Use normal, always visible scrollbar instead of "AutoScrollbar"
    This caused a bug in the message area. The last line was not shown if the Scrollbar appeared in the wrong moment.

--- gui/base.py
+++ gui/base.py
@@ -2,34 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-import tkinter
 import tkinter.ttk as ttk
 
 
-# Stolen from some demos #######################################################
-
-class AutoScrollbar(ttk.Scrollbar):
-    """A scrollbar that hides it self if it's not needed.
-    Only works if you use the grid geometry manager.
-    """
-    def set(self, lo, hi):
-        if float(lo) <= 0.0 and float(hi) >= 1.0:
-            self.grid_remove()
-        else:
-            self.grid()
-        super().set(lo, hi)
-
-    def pack(self, **kw):
-        raise tkinter.TclError("Can not use pack with this widget")
-
-    def place(self, **kw):
-        raise tkinter.TclError("Can not use place with this widget")
-
-################################################################################
-
-
 def setup_scrollbars(container, widget):
-    vsb = AutoScrollbar(container, orient="vertical", command=widget.yview)
+    vsb = ttk.Scrollbar(container, orient="vertical", command=widget.yview)
     widget.configure(yscrollcommand=vsb.set)
 
     widget.grid(column=0, row=0, sticky='nsew', in_=container)
