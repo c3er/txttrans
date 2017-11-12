913bce17bbb09c3c36aaad5305039a33521325f4
Christian Dreier <flipper.x3r@googlemail.com>
2017-11-12 04:01:33 +0100

    Approach did not fit to real use
    The executed script gets only objects that are globally available - this must be kept in mind.

--- spikes/executor.py
+++ spikes/executor.py
@@ -13,6 +13,9 @@ import msvcrt
 FILE = "executed.py"
 
 
+data = []
+
+
 def decorator(func):
     """A decorator with side effect to a data structure"""
     data.append(func.__name__)
@@ -23,26 +26,32 @@ def getstarterdir():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 
-oldcode = None
-file = os.path.join(getstarterdir(), FILE)
-while not msvcrt.kbhit():
-    try:
-        with open(file, encoding="utf8") as f:
-            codestr = f.read()
-        if codestr != oldcode:
-            print("Execute", file)
-            oldcode = codestr
-            code = compile(codestr, file, "exec")
+def main():
+    global data
+    oldcode = None
+    file = os.path.join(getstarterdir(), FILE)
+    while not msvcrt.kbhit():
+        try:
+            with open(file, encoding="utf8") as f:
+                codestr = f.read()
+            if codestr != oldcode:
+                print("Execute", file)
+                oldcode = codestr
+                code = compile(codestr, file, "exec")
 
-            # the data list needs to be initialized right before executing
-            data = []
+                # the data list needs to be initialized right before executing
+                data = []
 
-            # Execute the file with the global namespace...
-            exec(code)
-            for d in data:
-                print(d)
-    except:
-        print(traceback.format_exc(), file=sys.stderr)
-    finally:
-        time.sleep(1)
-msvcrt.getch()
+                # Execute the file with the global namespace...
+                exec(code)
+                for d in data:
+                    print(d)
+        except:
+            print(traceback.format_exc(), file=sys.stderr)
+        finally:
+            time.sleep(1)
+    msvcrt.getch()
+
+
+if __name__ == "__main__":
+    main()
